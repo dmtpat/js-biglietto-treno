15b2994bdd0600dfbaf10c290ac0f194225b481e
Pat desktop <dmtpatricia@gmail.com>
2025-10-27 22:14:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+*
+*js-biglietto-treno*
+*chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+*calcolare il prezzo totale del viaggio, secondo queste regole:
+* *il prezzo del biglietto è definito in base ai km (0.21 € al km)
+* *sconto del 20% per i minorenni
+* *sconto del 40% per gli over 65
+*risultato: un numero con due decimali dopo la virgola
+*stampare il risultato tramite un alert o in console
+*
+***Bonus: scrivete il risultato in un elemento in pagina (es. un tag <div>)***
+*
+*/
